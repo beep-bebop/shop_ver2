8241e9711f8032131124c34ff7ddcda906c2f419
Fun-You <weify369@gmail.com>
2020-04-02 15:53:14 +0800

    merge from zgy

---
+++ src/main/java/com/csu/shop/controller/CartController.java
@@ -0,0 +1,80 @@
+package com.csu.shop.controller;
+
+import com.csu.shop.domain.Cart;
+import com.csu.shop.domain.CartItem;
+import com.csu.shop.domain.Item;
+import com.csu.shop.service.CatalogService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Iterator;
+
+@Controller
+@RequestMapping("/cart")
+public class CartController {
+
+    @Autowired
+    private CatalogService catalogService;
+
+    @Autowired
+    private Cart cart;
+
+    @GetMapping("/viewCart")
+    public String viewCart(Model model){
+        model.addAttribute("cart",cart);
+        return "cart/cart";
+    }
+
+    @GetMapping("/addItemToCart")
+    public String addItemToCart(String workingItemId, Model model){
+        if(cart.containsItemId(workingItemId)){
+            cart.incrementQuantityByItemId(workingItemId);
+        }
+        else{
+            boolean isInStock = catalogService.isItemInStock(workingItemId);
+            Item item = catalogService.getItem(workingItemId);
+            cart.addItem(item,isInStock);
+        }
+        model.addAttribute("cart", cart);
+        return "cart/cart";
+    }
+
+    @GetMapping("/removeItemFromCart")
+    public String removeItemFromCart(String workingItem, Model model){
+        Item item = cart.removeItemById(workingItem);
+        if(item == null){
+            model.addAttribute("message","Attempted to remove null CartItem from Cart");
+            return "common/error";
+        }
+        else{
+            return "cart/cart";
+        }
+    }
+
+    @GetMapping("/updateCartQuantities")
+    public String updateCartQuantities(HttpServletRequest request, Model model){
+        Iterator<CartItem> cartItemIterator = cart.getAllCartItems();
+
+        while(cartItemIterator.hasNext()){
+            //产品数量增加
+            CartItem cartItem = cartItemIterator.next();
+            String itemId = cartItem.getItem().getItemId();
+
+            try {
+                int quantity = Integer.parseInt(request.getParameter(itemId));
+                cart.setQuantityByItemId(itemId, quantity);
+                if(quantity < 1){
+                    cartItemIterator.remove();
+                }
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+        }
+        model.addAttribute("cart", cart);
+        return "cart/cart";
+    }
+}
--- src/main/java/com/csu/shop/controller/CatalogController.java
+++ src/main/java/com/csu/shop/controller/CatalogController.java
@@ -1,14 +1,75 @@
 package com.csu.shop.controller;
 
+import com.csu.shop.domain.Item;
+import com.csu.shop.domain.Category;
+import com.csu.shop.domain.Product;
+import com.csu.shop.service.CatalogService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
 @Controller
-@RequestMapping("catalog")
+@RequestMapping("/catalog")
 public class CatalogController {
+
+    @Autowired
+    private CatalogService catalogService;
+
     @GetMapping("/main")
-    public String main() {
-        return "catalog/Main";
+    public String view(){
+        return "catalog/main";
+    }
+
+    @GetMapping("/viewCategory")
+    public String viewCategory(String categoryId, Model model){
+        if(categoryId != null){
+            Category category = catalogService.getCategory(categoryId);
+            List<Product> productList = catalogService.getProductListByCategory(categoryId);
+            model.addAttribute("category",category);
+            model.addAttribute("productList",productList);
+            return "catalog/category";
+        }
+        return "catalog/main";
+    }
+
+    @GetMapping("/viewProduct")
+    public String viewProduct(String productId, Model model){
+        Product product = catalogService.getProduct(productId);
+        List<Item> itemList = catalogService.getItemListByProduct(productId);
+        model.addAttribute("product",product);
+        model.addAttribute("itemList",itemList);
+        return "catalog/product";
+    }
+
+    @GetMapping("/viewItem")
+    public String viewItem(String itemId, Model model){
+        Item item = catalogService.getItem(itemId);
+        model.addAttribute("item",item);
+        return "catalog/item";
+    }
+
+    @PostMapping("serchProducts")
+    public String serchProducts(String keyword, Model model){
+        if(keyword == null || keyword.length() < 1){
+            String msg = "Please enter a keyword to search for, then press the search button.";
+            model.addAttribute("msg",msg);
+            return "common/error";
+        }else {
+            List<Product> productList = catalogService.searchProductList(keyword.toLowerCase());
+            processProductDescription(productList);
+            model.addAttribute("productList",productList);
+            return "catalog/search_products";
+        }
+    }
+
+    /*
+        待完成方法
+     */
+    private void processProductDescription(List<Product> productList) {
     }
 }
