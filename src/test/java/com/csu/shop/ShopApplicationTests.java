375d604f6e5f1dbc6dede50ba2ed4a4871608a2f
Fun-You <weify369@gmail.com>
2020-04-03 19:15:37 +0800

    完善了order页面

--- src/test/java/com/csu/shop/ShopApplicationTests.java
+++ src/test/java/com/csu/shop/ShopApplicationTests.java
@@ -1,7 +1,8 @@
 package com.csu.shop;
 
-import com.csu.shop.domain.Cart;
+import com.csu.shop.domain.Product;
 import com.csu.shop.service.CartService;
+import com.csu.shop.service.CatalogService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -10,10 +11,17 @@ import org.springframework.boot.test.context.SpringBootTest;
 class ShopApplicationTests {
     @Autowired
     private CartService cartService;
+    @Autowired
+    private CatalogService catalogService;
 
     @Test
     void contextLoads() {
-        Cart cart = cartService.getCart("wfy");
-        System.out.print(cart.getItemList().get(0).getTotal()+cart.getItemList().get(0).getItemId()+cart.getItemList().get(0).getQuantity());
+        Product product = catalogService.getProduct("AV-SB-02");
+        if(product != null)
+            System.out.println("BBBBBBBBBBB"+product.getProductId()+product.getCategoryId());
+        else
+            System.out.println("zhaobudaoaaaaaaaaaaaaa");
+//        Cart cart = cartService.getCart("wfy");
+//        System.out.print(cart.getItemList().get(0).getTotal()+cart.getItemList().get(0).getItemId()+cart.getItemList().get(0).getQuantity());
     }
 }
